8f4447ebd7fd84e8d7de8fc3913eb17a27efe295
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2014-12-10 18:28:26 +0100

    cleaning up the file

--- NetCeylonBeans/src/org/ncb/error/CeylonSyntaxErrorHighlightingTask.java
+++ NetCeylonBeans/src/org/ncb/error/CeylonSyntaxErrorHighlightingTask.java
@@ -1,22 +1,20 @@
 package org.ncb.error;
+
 import com.redhat.ceylon.compiler.typechecker.parser.ParseError;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import javax.swing.text.Document;
-import javax.swing.text.StyledDocument;
 import org.ncb.parser.NBCeylonParser.CeylonParserResult;
 import org.netbeans.modules.parsing.spi.Parser.Result;
 import org.netbeans.modules.parsing.spi.ParserResultTask;
 import org.netbeans.modules.parsing.spi.Scheduler;
 import org.netbeans.modules.parsing.spi.SchedulerEvent;
 import org.netbeans.spi.editor.hints.ErrorDescription;
-import org.netbeans.spi.editor.hints.ErrorDescriptionFactory;
 import org.netbeans.spi.editor.hints.HintsController;
-import org.netbeans.spi.editor.hints.Severity;
-import org.openide.text.NbDocument;
 import org.openide.util.Exceptions;
+
 public class CeylonSyntaxErrorHighlightingTask extends ParserResultTask {
+
     @Override
     public void run(Result result, SchedulerEvent event) {
         try {
@@ -51,14 +49,17 @@ public class CeylonSyntaxErrorHighlightingTask extends ParserResultTask {
             Exceptions.printStackTrace(ex1);
         }
     }
+
     @Override
     public int getPriority() {
         return 100;
     }
+
     @Override
     public Class getSchedulerClass() {
         return Scheduler.EDITOR_SENSITIVE_TASK_SCHEDULER;
     }
+
     @Override
     public void cancel() {
     }
